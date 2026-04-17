e8d25a8178a6fc2bf4a81bb93e0be5ee1dd16e0b
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-17 17:09:59 +0530

    feat: initial project setup

---
+++ src/controllers/note.controller.js
@@ -0,0 +1,3 @@
+const Note = require("../models/note.model.js");
+
+// Endpoints will be added here step by step
---
+++ src/routes/note.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+// const { ... } = require("../controllers/note.controller");
+
+// Routes will be added here step by step
+
+module.exports = router;
